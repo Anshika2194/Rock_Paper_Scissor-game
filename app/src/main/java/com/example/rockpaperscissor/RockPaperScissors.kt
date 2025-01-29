9bfbb183855dc233ba9682f452fbc79bb8d8cfcc
Anshika2194 <srianshika2194@gmail.com>
2025-01-29 12:21:17 +0530

    Initialize project with basic setup

---
+++ app/src/main/java/com/example/rockpaperscissor/RockPaperScissors.kt
@@ -0,0 +1,44 @@
+package com.example.rockpaperscissor
+
+fun main() {
+
+    var computerChoice = ""
+    var playerChoice = ""
+    println("Rock,Paper or Scissors? Enter your choice!")
+    playerChoice = readln()
+    val randomNumber = (1..3).random()
+    when (randomNumber) {
+        1 -> {
+            computerChoice = "Rock"
+        }
+
+        2 -> {
+            computerChoice = "Paper"
+        }
+
+        3 -> {
+            computerChoice = "Scissors"
+        }
+    }
+    println(computerChoice)
+    val winner = when {
+        playerChoice == computerChoice -> "Tie"
+        playerChoice == "Rock" && computerChoice == "Scissors" -> "Player"
+        playerChoice == "Paper" && computerChoice == "Rock" -> "Player"
+        playerChoice == "Scissors" && computerChoice == "Paper" -> "Player"
+        else -> "Computer"
+    }
+    if(winner == "Tie"){
+        println("It's a tie")
+    }
+    else if(winner == "Player"){
+        println("Player won")
+    }
+    else{
+        println("Computer won")
+    }
+
+}
+
+
+
